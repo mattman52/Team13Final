79a33c1f6ee970b235fbcbeac38820c92ab991d1
josepden0 <denis.jose.joseph@gmail.com>
2021-11-08 12:40:05 -0500

    Assignor page backend and website

---
+++ app/public/api/assignor/index.php
@@ -0,0 +1,30 @@
+<?php
+// require 'common.php';
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM games';
+$vars = [];
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$games = $stmt->fetchAll();
+$sql = 'SELECT * FROM referees';
+$vars = [];
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$referees = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$result = array( 'ref' => $referees, 'games' => $games );
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo json_encode( $result );
+
